b9e262f058e223d606471c99ac84d4075b4c78c2
ahernandez <ahernandez@mdurance.es>
2021-05-17 15:25:00 +0200

    Add contents

---
+++ api/contents/models.py
@@ -0,0 +1,30 @@
+from channels.models import Channel
+from core.models import PublicIdModel, TimeStampedModel, TitleModel
+from core.models.image_models import content_file_name
+from django.db import models
+from envs.models import Attributes
+
+
+class Content(
+    TimeStampedModel, PublicIdModel, TitleModel
+):
+
+    channel = models.ForeignKey(
+        Channel,
+        models.DO_NOTHING,
+        related_name='channels',
+        blank=False, null=False
+    )
+
+    kind = models.ForeignKey(
+        Attributes,
+        models.DO_NOTHING,
+        related_name='kinds',
+        limit_choices_to={'envs_type_name': 'content'}
+    )
+
+    content = models.FileField(
+        upload_to=content_file_name,
+    )
+
+    src = models.JSONField()
--- api/immfly/settings.py
+++ api/immfly/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # Appa
     'envs',
     'channels',
+    'contents',
 ]
 
 MIDDLEWARE = [
