fbd8c57328a5d50ed05b9c20f9c489a325a3db17
ahernandez <ahernandez@mdurance.es>
2021-05-20 11:35:57 +0200

    Update channels for models and include migration

--- api/channels/models.py
+++ api/channels/models.py
@@ -1,6 +1,11 @@
+import logging
+
+from channels.managers import ChannelManager
 from core.models import ImageModel, PublicIdModel, TimeStampedModel, TitleModel
 from django.db import models
 from envs.models import Attributes
+
+logger = logging.getLogger(__name__)
 
 
 class Channel(
@@ -11,7 +16,7 @@ class Channel(
         Attributes,
         models.DO_NOTHING,
         related_name='languages',
-        limit_choices_to={'envs_type_name': 'languages'}
+        limit_choices_to={'envs_type__name': 'languages'}
     )
 
     tree_channel = models.ForeignKey(
@@ -20,3 +25,28 @@ class Channel(
         related_name='tree_channels',
         blank=True, null=True
     )
+
+    objects = ChannelManager()
+
+    @property
+    def score(self):
+        """Return score from channel.
+        Return:
+            Float: score
+        """
+        value = self._calculate_sinlge_score(
+            channel_id=self.id
+        )
+        return round(value.average, 2)
+
+    @classmethod
+    def _calculate_sinlge_score(cls, channel_id):
+        """Calculate score from one channel.
+        Args:
+            channel_id(int): Channel id
+        Return:
+            Queryset: queryset.
+        """
+        return cls.objects.filter(
+            id=channel_id
+        ).calculate_score().last()
