02de1e12e97f254f79b9a4f00e8456aa86a24750
Alejandro <jose.alejandro.her.ros@gmail.com>
2021-05-20 17:57:06 +0200

    Solved problems with old solved problems

--- api/envs/management/commands/first_command.py
+++ api/envs/management/commands/first_command.py
@@ -1,6 +1,6 @@
 from django.core import management
 from django.core.management.base import BaseCommand
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Command(BaseCommand):
