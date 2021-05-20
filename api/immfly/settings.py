797849e5e39253fc39e4ad17cb65f1cbd75f99e0
ahernandez <ahernandez@mdurance.es>
2021-05-20 10:26:20 +0200

    solved problem with media root

--- api/immfly/settings.py
+++ api/immfly/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # long to public ID
 LONG_PUBLIC_ID = 15
